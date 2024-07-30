1be4f41ac8fc494567527c49ff5f674dce8e5d67
zeeshan ali anjam <zeeshanalianjam49@gmail.com>
2024-07-30 08:57:37 +0500

    how to set class for api error from the help api error site using class for extends method and in the use contructor and export

---
+++ professionalsetup/src/utils/apiError.js
@@ -0,0 +1,25 @@
+class ApiError extends ERROR{
+    constructor(
+        statusCode,
+        message = "something went wrong! ",
+        erros = [],
+        stack,
+    ){
+        super(message)
+        this.statusCode = statusCode,
+        this.data = null,
+        this.erros = erros,
+        this.success = false,
+        this.message = message
+
+        if(stack){
+            this.stack = stack
+        }else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+
+    }
+}
+
+
+export {ApiError}
