987fb5cf42b52009ce5991e310526b412d256aab
zeeshan ali anjam <zeeshanalianjam49@gmail.com>
2024-07-30 08:51:58 +0500

    setting first of all main root index file direct db connect with iffee function  using with async await and try catch

--- professionalsetup/src/index.js
+++ professionalsetup/src/index.js
@@ -1,2 +1,51 @@
+// import mongoose from 'mongoose'
+// import DB_NAME from './constants'
+// import express from 'express'
+// const app = express()
+import dotenv from 'dotenv'
+import DBConnect from './db/index.js'
 
-console.log("Hey i'm just checking nodemon")
+dotenv.config({
+    path: './.env'
+})
+
+
+
+DBConnect()
+.then(() => {
+    app.on('Error', (ERR) => {
+        console.log(`Error from Express App Listener :: ${ERR}`);
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Port listening on ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log(`Mongo db Connection Error !!! ${error} `);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// ;( async () => {
+//     try {
+//        await mongoose.connection(`${process.env.MONOGODB_URL}/${DB_NAME}`)
+//         app.on("errorr", (error) => {
+//             console.log("ERROR FROM EXPRESS APP LISTENER ", error );
+//         })
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on PORT ${process.env.PORT}`);
+//         })
+//     } catch (error) {
+//         console.log("ERROR in catch ", error)
+
+//     }
+// })()
