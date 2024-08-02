5c8b9a01a0c6da9466d26119100ad7e8eca7c661
zeeshan ali anjam <zeeshanalianjam49@gmail.com>
2024-08-02 11:05:44 +0500

    added user already exists or not

--- professionalsetup/src/controllers/user.controllers.js
+++ professionalsetup/src/controllers/user.controllers.js
@@ -1,8 +1,9 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
+import { User } from '../models/user.models.js'
 
 
-const registerUser = asyncHandler( async (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
     // first request check in postman with status code and ok message
     //  res.status(200).json({
     //     message: "ok",
@@ -11,17 +12,28 @@ const registerUser = asyncHandler( async (req, res) => {
 
 
     // get details from user 
-    const {username, email, fullname, password} =  req.body
+    const { username, email, fullname, password } = req.body
     console.log('Email: ', email);
     console.log('Password: ', password);
 
     // validation setting
-    if ([username, email, fullname, password].some((fields) => 
+    if ([username, email, fullname, password].some((fields) =>
         fields?.trim() === ""
-)) {
+    )) {
         throw new ApiError(400, "All fields are required ")
     }
-    
+
+    // check user already exist or not
+    const userExist = User.findOne({
+        $or: [{username, email}]
+    })
+
+    if (userExist) {
+        throw new ApiError(409, "User with email or username already exists!")
+        
+    }
+
+
 
 
 })
