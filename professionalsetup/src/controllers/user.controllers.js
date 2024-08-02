912d2e775e155ef65942395c1488b3ae1adf2351
zeeshan ali anjam <zeeshanalianjam49@gmail.com>
2024-08-02 10:31:43 +0500

    now register user process start here first of all use controllers  in get details from user with the help of deconstrct method

--- professionalsetup/src/controllers/user.controllers.js
+++ professionalsetup/src/controllers/user.controllers.js
@@ -1,9 +1,20 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async (req, res) => {
-     res.status(200).json({
-        message: "ok",
-    })
+    // first request check in postman with status code and ok message
+    //  res.status(200).json({
+    //     message: "ok",
+    // })
+
+
+
+    // get details from user 
+    const {username, email, fullname, password} =  req.body
+    console.log('Email: ', email);
+    console.log('Password: ', password);
+    
+
+
 })
 
 export default registerUser;
