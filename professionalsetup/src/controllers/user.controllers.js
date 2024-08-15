6b1c18917a7ad794954ef41c88154ad2cff995b8
ZeeshanAliAnjam <zeeshanalianjam49@gmail.com>
2024-08-15 13:49:36 +0500

    api response update with avatar to user

--- professionalsetup/src/controllers/user.controllers.js
+++ professionalsetup/src/controllers/user.controllers.js
@@ -376,7 +376,7 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
 
     return res
        .status(200)
-       .json(new ApiResponse(200, avatar, 'avatar has been uploaded'))
+       .json(new ApiResponse(200, user, 'avatar has been uploaded'))
 })
 
 export { registerUser, loginUser, logoutUser, refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails }
